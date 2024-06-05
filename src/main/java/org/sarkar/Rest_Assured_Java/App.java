88f1836e97c83d702fe6c598fef5354a8c5a3703
Shubha Sarkar <53209433+shubha-sarkar@users.noreply.github.com>
2024-06-05 12:25:09 +0530

    Initial commit

---
+++ src/main/java/org/sarkar/Rest_Assured_Java/App.java
@@ -0,0 +1,13 @@
+package org.sarkar.Rest_Assured_Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
